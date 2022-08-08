bd94963ab56701a1448de9bfc95dffdcd0f09ce5
mango57777 <87117779+mango57777@users.noreply.github.com>
2022-08-08 11:12:09 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -2,5 +2,5 @@
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p
-green "出现bbr字段则成功"
+echo "出现bbr字段则成功"
 lsmod | grep bbr
